0b177552dbb34608dccd3b8be48b5fc3e66753ea
Shannon Lewis <slewis74@gmail.com>
2016-11-10 11:01:28 +1000

    Update so that IsLocalUrl allows absolute paths, so long as they are from the same directoryPath (i.e. virtual directory)

---
+++ source/DirectoryServices.Tests/IntegratedAuthenticationRedirectUrlTests.cs
@@ -0,0 +1,37 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Octopus.Diagnostics;
+using Octopus.Server.Extensibility.Authentication.DirectoryServices;
+using Octopus.Server.Extensibility.Authentication.HostServices;
+using Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Api;
+
+namespace DirectoryServices.Tests
+{
+    [TestFixture]
+    public class IntegratedAuthenticationRedirectUrlTests
+    {
+        [Test]
+        public void NonLocalRedirectUrlCausesAWarningToBeLogged()
+        {
+            var log = Substitute.For<ILog>();
+            var module = new IntegratedAuthenticationModule(log, Substitute.For<IAuthCookieCreator>(), Substitute.For<IApiActionResponseCreator>());
+
+            module.IsLocalUrl("http://site1", "http://anotherSite");
+
+            log.Received(1).WarnFormat(Arg.Any<string>(), "http://site1", "http://anotherSite");
+        }
+
+        [TestCase("http://site1", "/folder1/api", ExpectedResult = true)]
+        [TestCase("http://site1", "~/folder1/api", ExpectedResult = true)]
+        [TestCase("http://site1", "http://site1/folder1/api", ExpectedResult = true)]
+        [TestCase("http://site1", "folder1/api", ExpectedResult = false)]
+        [TestCase("http://site1", "http://site2/folder1/api", ExpectedResult = false)]
+        public bool IsPathLocalTest(string directoryPath, string url)
+        {
+            var log = Substitute.For<ILog>();
+            var module = new IntegratedAuthenticationModule(log, Substitute.For<IAuthCookieCreator>(), Substitute.For<IApiActionResponseCreator>());
+
+            return module.IsLocalUrl(directoryPath, url);
+        }
+    }
+}
--- source/Octopus.Server.Extensibility.Authentication.DirectoryServices/IntegratedAuthenticationModule.cs
+++ source/Octopus.Server.Extensibility.Authentication.DirectoryServices/IntegratedAuthenticationModule.cs
@@ -1,5 +1,6 @@
 using Nancy;
 using Nancy.Responses;
+using Octopus.Diagnostics;
 using Octopus.Server.Extensibility.Authentication.DirectoryServices.DirectoryServices;
 using Octopus.Server.Extensibility.Authentication.HostServices;
 using Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Api;
@@ -9,8 +10,12 @@ namespace Octopus.Server.Extensibility.Authentication.DirectoryServices
 {
     public class IntegratedAuthenticationModule : NancyModule
     {
-        public IntegratedAuthenticationModule(IAuthCookieCreator tokenIssuer, IApiActionResponseCreator responseCreator)
+        readonly ILog log;
+
+        public IntegratedAuthenticationModule(ILog log, IAuthCookieCreator tokenIssuer, IApiActionResponseCreator responseCreator)
         {
+            this.log = log;
+
             Get[DirectoryServicesConstants.ChallengePath] = c =>
             {
                 if (Context.CurrentUser == null)
@@ -20,9 +25,9 @@ namespace Octopus.Server.Extensibility.Authentication.DirectoryServices
                 var tokenCookie = tokenIssuer.CreateAuthCookie(Context, principal.IdentificationToken, false);
 
                 Response response;
-                if (Request.Query["redirectTo"].HasValue && IsLocalUrl(Request.Query["redirectTo"].Value))
+                if (Request.Query["redirectTo"].HasValue && IsLocalUrl(Request.DirectoryPath(), Request.Query["redirectTo"].Value))
                 {
-                    var redirectLocation = Request.DirectoryPath() + "/app#" + Request.Query["redirectTo"].Value;
+                    var redirectLocation = Request.Query["redirectTo"].Value;
                     response = new RedirectResponse(redirectLocation).WithCookie(tokenCookie);
                 }
                 else
@@ -34,18 +39,27 @@ namespace Octopus.Server.Extensibility.Authentication.DirectoryServices
             };
         }
 
-        public bool IsLocalUrl(string url)
+        public bool IsLocalUrl(string directoryPath, string url)
         {
             // Credit to Microsoft - Preventing Open Redirection Attacks (C#)
             // http://www.asp.net/mvc/overview/security/preventing-open-redirection-attacks
 
-            return !string.IsNullOrEmpty(url) &&
+            var isLocalUrl = !string.IsNullOrEmpty(url) &&
+                            
+                             // Allows full paths that start with the DirectoryPath
+                             (url.StartsWith(directoryPath) ||
 
-                // Allows "/" or "/foo" but not "//" or "/\".
-                ((url[0] == '/' && (url.Length == 1 || (url[1] != '/' && url[1] != '\\'))) ||
+                             // Allows "/" or "/foo" but not "//" or "/\".
+                             (url[0] == '/' && (url.Length == 1 || (url[1] != '/' && url[1] != '\\'))) ||
 
-                // Allows "~/" or "~/foo".
-                (url.Length > 1 && url[0] == '~' && url[1] == '/'));
+                              // Allows "~/" or "~/foo".
+                              (url.Length > 1 && url[0] == '~' && url[1] == '/'));
+            if (!isLocalUrl)
+            {
+                log.WarnFormat("Prevented potential Open Redirection attack on an NTLM challenge from the local instance {0} to the non-local url {1}", directoryPath, url);
+            }
+
+            return isLocalUrl;
         }
     }
 }
