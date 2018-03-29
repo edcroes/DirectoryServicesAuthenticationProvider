05138665435fe76a54b81901df031dffd8ad8a31
Michael Noonan <michaelnoonan@gmail.com>
2018-03-29 12:11:43 +1000

    Allow the state to be null or invalid
    We should just fall back to the root of the web site.

--- source/Server/IntegratedAuthenticationModule.cs
+++ source/Server/IntegratedAuthenticationModule.cs
@@ -23,33 +23,45 @@ namespace Octopus.Server.Extensibility.Authentication.DirectoryServices
 
                 var principal = (IOctopusPrincipal)Context.CurrentUser;
 
+                // Decode the state object sent from the client (if there was one) so we can use those hints to build the most appropriate response
+                // If the state can't be interpreted, we will fall back to a safe-by-default behaviour, however:
+                //   1. Deep-links will not work because we don't know where the anonymous request originally wanted to go
+                //   2. Cookies may not have the Secure flag set properly when SSL Offloading is in play
+                LoginState state = null;
                 if (Request.Query["state"].HasValue)
                 {
-                    var stateData = HttpUtility.UrlDecode((string)Request.Query["state"].Value);
-                    var state = JsonConvert.DeserializeObject<LoginState>(stateData);
-
-                    var authCookies = tokenIssuer.CreateAuthCookies(Context.Request, principal.IdentificationToken, SessionExpiry.TwentyDays, state.UsingSecureConnection);
-
-                    var whitelist = authenticationConfigurationStore.GetTrustedRedirectUrls();
-
-                    if (Requests.IsLocalUrl(state.RedirectAfterLoginTo, whitelist))
+                    try
                     {
-                        return new RedirectResponse(state.RedirectAfterLoginTo).WithCookies(authCookies);
+                        var stateString = HttpUtility.UrlDecode((string) Request.Query["state"].Value);
+                        state = JsonConvert.DeserializeObject<LoginState>(stateString);
                     }
-                    else
+                    catch (Exception e)
                     {
-                        if (!string.IsNullOrWhiteSpace(state.RedirectAfterLoginTo))
-                        {
-                            log.WarnFormat(
-                                "Prevented potential Open Redirection attack on an NTLM challenge, to the non-local url {0}",
-                                state.RedirectAfterLoginTo);
-                        }
-
-                        return new RedirectResponse(Request.Url.BasePath ?? "/").WithCookies(authCookies);
+                        log.Warn(e, "Invalid login state object passed to the server when setting up the NTLM challenge. Falling back to the default behaviour.");
                     }
                 }
 
-                return responseCreator.BadRequest("Invalid state passed to the server when setting up the NTLM challenge.");
+                // Build the auth cookies to send back with the response
+                var authCookies = tokenIssuer.CreateAuthCookies(Context.Request, principal.IdentificationToken, SessionExpiry.TwentyDays, state?.UsingSecureConnection);
+
+                // If the caller has provided a redirect after successful login, we need to check it is a local address - preventing Open Redirection attacks
+                if (!string.IsNullOrWhiteSpace(state?.RedirectAfterLoginTo))
+                {
+                    var whitelist = authenticationConfigurationStore.GetTrustedRedirectUrls();
+                    if (Requests.IsLocalUrl(state.RedirectAfterLoginTo, whitelist))
+                    {
+                        // This is a safe redirect, let's go!
+                        return new RedirectResponse(state.RedirectAfterLoginTo).WithCookies(authCookies);
+                    }
+
+                    // Just log that we detected a non-local redirect URL, and fall through to the root of the local web site
+                    log.WarnFormat(
+                        "Prevented potential Open Redirection attack on an NTLM challenge, to the non-local url {0}",
+                        state.RedirectAfterLoginTo);
+                }
+
+                // By default, redirect to the root of the local web site
+                return new RedirectResponse(Request.Url.BasePath ?? "/").WithCookies(authCookies);
             };
         }
     }
