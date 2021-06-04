52d4649f657d96cb1d559e19b92d184d2eb9a88d
Shane <tothegills@gmail.com>
2021-06-04 15:15:00 +1000

    Stop ignoring the build folder

---
+++ build/Build.cs
@@ -0,0 +1,123 @@
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using OctoVersion.Core;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using Nuke.OctoVersion;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class Build : NukeBuild
+{
+    readonly Configuration Configuration = Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+
+    [NukeOctoVersion] readonly OctoVersionInfo OctoVersionInfo;
+
+    static AbsolutePath SourceDirectory => RootDirectory / "source";
+    static AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    static AbsolutePath PublishDirectory => RootDirectory / "publish";
+    static AbsolutePath LocalPackagesDir => RootDirectory / ".." / "LocalPackages";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj", "**/TestResults").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(ArtifactsDirectory);
+            EnsureCleanDirectory(PublishDirectory);
+        });
+
+    Target CalculateVersion => _ => _
+        .Executes(() =>
+        {
+            //all the magic happens inside `[NukeOctoVersion]` above. we just need a target for TeamCity to call
+        });
+
+    Target Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetRestore(_ => _
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Clean)
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            Logger.Info("Building Directory Services Authentication Provider v{0}", OctoVersionInfo.FullSemVer);
+
+            DotNetBuild(_ => _
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetVersion(OctoVersionInfo.FullSemVer)
+                .EnableNoRestore());
+        });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(_ => _
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetNoBuild(true)
+                .EnableNoRestore());
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Test)
+        .Produces(ArtifactsDirectory / "*.nupkg")
+        .Executes(() =>
+        {
+            Logger.Info("Packing Directory Services Authentication Provider v{0}", OctoVersionInfo.FullSemVer);
+
+            DotNetPack(_ => _
+                .SetProject(Solution)
+                .SetVersion(OctoVersionInfo.FullSemVer)
+                .SetConfiguration(Configuration)
+                .SetOutputDirectory(ArtifactsDirectory)
+                .EnableNoBuild()
+                .DisableIncludeSymbols()
+                .SetVerbosity(DotNetVerbosity.Normal)
+                .SetProperty("NuspecFile", "../../build/Octopus.Server.Extensibility.Authentication.DirectoryServices.nuspec")
+                .SetProperty("NuspecProperties", $"Version={OctoVersionInfo.NuGetVersion}"));
+        });
+
+    Target CopyToLocalPackages => _ => _
+        .OnlyWhenStatic(() => IsLocalBuild)
+        .TriggeredBy(Pack)
+        .Executes(() =>
+        {
+            EnsureExistingDirectory(LocalPackagesDir);
+            ArtifactsDirectory.GlobFiles("*.nupkg")
+                .ForEach(package => CopyFileToDirectory(package, LocalPackagesDir));
+        });
+
+    Target OutputPackagesToPush => _ => _
+        .DependsOn(Pack)
+        .Executes(() =>
+        {
+            ArtifactsDirectory.GlobFiles("*.nupkg")
+                .NotEmpty()
+                .ForEach(package => System.Console.WriteLine($"::set-output name=packages_to_push::{package}"));
+        });
+
+    Target Default => _ => _
+        .DependsOn(OutputPackagesToPush);
+
+    /// Support plugins are available for:
+    /// - JetBrains ReSharper        https://nuke.build/resharper
+    /// - JetBrains Rider            https://nuke.build/rider
+    /// - Microsoft VisualStudio     https://nuke.build/visualstudio
+    /// - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Default);
+}
